298b55c4078faacbb89a9d4b2153281d73632687
vignesh1214 <vignesh1214@gmail.com>
2018-06-16 00:22:34 -0700

    text changes

--- tripplanner.py
+++ tripplanner.py
@@ -12,7 +12,7 @@ amadeus = Client(
 def date_entry(dates):
 	year, month, day = map(int, dates.split('-'))
 	final_date = date(year, month, day)
-	return final_date
+	yield final_date
 
 def date_range(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
@@ -33,7 +33,7 @@ def flightsearch(startDate,endDate,duration,origin,destination):
 		price = response.data[0].get('offerItems')[0].get('price')
 		total = float(price.get('total')) + float(price.get('totalTaxes'))
 		#print(total)
-		dateprice[startingDate + '--' + returningDate]=total
+		dateprice[startingDate + '-' + returningDate]=total
 	return dateprice
 	
 
@@ -49,7 +49,7 @@ duration = input("Duration of stay at each place- ")
 flight_list={}
 for i in range(0,trips):
 	df= pd.Series(flightsearch(date_entry(origindate),date_entry(enddate),duration,origin,destination[i]))
-	flight_list[origin +'-' + destination[i]]=df.sort_values().head(3)
+	flight_list[origin +'--' + destination[i]]=df.sort_values().head()
 	#print(df.sort_values().head(3))
 
 
@@ -57,37 +57,5 @@ for i in range(0,trips):
 #df = pd.Series(dateprice)
 #print(df.sort_values().head(3))
 print(flight_list)
-
-
-# inputdate = input("Date of Travel starting range -- July? ")
-# enddate = input("Return before -- July? ")
-# duration = input("Duration of stay - ")
-# startDate= "2018-07-"+str(inputdate)
-# endDate = "2018-07-"+str(enddate)
-# for i in range(int(inputdate),int(enddate)):
-# 	x='2018-07-'+str(i)
-# 	y='2018-07-'+str(i+int(duration))
-# 	response = amadeus.shopping.flight_offers.get(origin='SFO', destination='LAX', departureDate=x,returnDate=y,max=1)
-# 		#data = response.data[0].get('offerItems')[0].get('price')
-# 		#print(data[0].get('offerItems')[0].get('price'))
-# 	price = response.data[0].get('offerItems')[0].get('price')
-# 	total = float(price.get('total')) + float(price.get('totalTaxes'))
-# 	#print(total)
-# 	dateprice['2018:07:'+str(i)+'--'+'2018:07:'+str(i+int(duration))]=total
-
-
-# df = pd.Series(dateprice)
-# print(df.sort_values().head(3))
-#print(min(dateprice, key=dateprice.get))
-# json.loads(response.data)
-
-# print(response.data)
-# data = response.data
-# print(data[0].get('offerItems')[0].get('price'))
-# price = data[0].get('offerItems')[0].get('price')
-# total = float(price.get('total')) + float(price.get('totalTaxes'))
-# print(total)
-# print("total: " + price.get('total'))
-# print("totalTaxes: " + price.get('totalTaxes'))
 
 
